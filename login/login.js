752b0d03f49ad8c195744cb8eba51d0a4eb21fd6
Muhammad Meeral <mmeeral.xwave@gmail.com>
2024-09-19 11:07:29 +0500

    assignment compleate

---
+++ login/login.js
@@ -0,0 +1,26 @@
+document.getElementById('loginForm')
+.addEventListener('submit', async function (e)  {
+  e.preventDefault();
+  const useremail = document.getElementById('userEmail').value;
+  const userpassword = document.getElementById('userPassword').value;
+
+  try {
+      // Fetching all users from the API
+      const response = await fetch('https://fakestoreapi.com/users');
+      const users = await response.json();
+
+      // Check if the user exists and password matches
+      const user = users.find(user => user.email === useremail && user.password === userpassword);
+
+      if (user) {
+          alert('Login successful!');
+          window.location.href = "./../index.html"
+          // Redirect or do something after successful login
+      } else {
+          alert('Invalid username or password.');
+      }
+  } catch (error) {
+      alert('Error logging in.');
+      console.error(error);
+  }
+});
---
+++ sign-in/signin.js
@@ -0,0 +1,31 @@
+document.getElementById('signupForm')
+.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const useremail = document.getElementById('signup-useremail').value;
+    const password = document.getElementById('signup-password').value;
+
+    // Create a new user
+    const user = {
+        useremail: useremail,
+        password: password,
+        // You can add more user fields as required by the API (e.g., name, email, address)
+    };
+
+    try { 
+        const response = await fetch('https://fakestoreapi.com/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        });
+  
+        const data = await response.json();
+        alert('User created successfully. Now you can log in.');
+        window.location.href = "./../index.html"
+        console.log(data);
+    } catch (error) {
+        alert('Error creating user.');
+        console.error(error);
+    }
+});
